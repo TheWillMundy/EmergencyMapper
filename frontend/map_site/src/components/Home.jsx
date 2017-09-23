6ac04732d36b19c502d6a7d4457c8971a6b63c79
ThunderLight25 <cogsteeler2@gmail.com>
2017-09-22 23:29:58 -0500

    Added basic google map

--- frontend/map_site/src/components/Home.jsx
+++ frontend/map_site/src/components/Home.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 //Material UI Imports
 import AppBar from 'material-ui/AppBar'
+import MapToExport from './GoogleMap.jsx'
 
 class Home extends Component {
   render() {
     return (
       <div>
+        <MapToExport />
         <h1>Homepage</h1>
       </div>
     );
