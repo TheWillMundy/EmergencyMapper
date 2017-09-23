958b2088077d57a3a659c96924662102d5e0c41c
GaiSawant <31870118+GaiSawant@users.noreply.github.com>
2017-09-22 23:32:16 -0500

    Add files via upload

---
+++ emergencymapper/emergencymapper/Filter Streaming Tweets.py
@@ -0,0 +1,32 @@
+import tweepy
+from tweepy import OAuthHandler
+from tweepy import Stream
+from tweepy.streaming import StreamListener
+
+consumer_key = ''
+consumer_secret = ''
+access_token = ''
+access_secret = ''
+
+auth = OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_secret)
+
+api = tweepy.API(auth)
+
+class MyListener(StreamListener):
+ 
+    def on_data(self, data):
+        try:
+            with open('python.json', 'a') as f:
+                f.write(data)
+                return True
+        except BaseException as e:
+            print("Error on_data: %s" % str(e))
+        return True
+ 
+    def on_error(self, status):
+        print(status)
+        return True
+ 
+twitter_stream = Stream(auth, MyListener())
+twitter_stream.filter(track=['#harvey'])
