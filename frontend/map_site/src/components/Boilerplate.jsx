60b84bff6ae77799508ce0c2ae07b6f851a24b23
ThunderLight25 <cogsteeler2@gmail.com>
2017-09-22 22:28:14 -0500

    Added components folder

---
+++ frontend/map_site/src/components/Boilerplate.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Boilerplate extends Component {
+  render() {
+    return (
+      <div>
+        <p>I'm a boilerplate!</p>
+      </div>
+    );
+  }
+}
+
+export default Boilerplate;
